8f976ec96e4b4254ac241249b235f41d322fd3fc
toratommy <tommy@torainsights.ai>
2024-04-19 12:29:56 -0500

    add fragment decorator

--- src/utils.py
+++ src/utils.py
@@ -15,6 +15,15 @@ import osmnx as ox
 from folium.plugins import HeatMap
 from shapely.geometry import mapping
 
+@st.experimental_fragment
+def make_catchment_area_selections():
+    st.subheader('Get started: define your catchment area')
+    address = st.text_input("Enter the Address", value='2834 N. Ashland Ave, Chicago, IL 60657')
+    radius_type = st.selectbox("Enter Radius Type", ["Distance (miles)", "Drive Time (minutes)"], index = 1)
+    radius = st.number_input(f"Enter Radius in {radius_type.split()[1]}", min_value=1, max_value = 100, value=10)
+    generate_catchment = st.button("Generate Catchment Area")
+    return address, radius_type, radius, generate_catchment
+
 def draw_circle(catchment_map, location, radius):
     """
     Draws a circle on a map at a specified location and radius.
