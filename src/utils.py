95487a0bdee5d9e3b8f7fce61b3517fb45485ee8
toratommy <tommy@torainsights.ai>
2024-04-20 14:52:58 -0500

    update radius type

--- src/utils.py
+++ src/utils.py
@@ -19,7 +19,7 @@ from shapely.geometry import mapping
 def make_catchment_area_selections():
     address = st.text_input("Enter the Address", value='2834 N. Ashland Ave, Chicago, IL 60657')
     radius_type = st.selectbox("Enter Radius Type", ["Distance (miles)", "Drive Time (minutes)"], index = 1)
-    radius = st.number_input(f"Enter Radius in {radius_type.split()[1]}", min_value=1, max_value = 100, value=10)
+    radius = st.number_input(f"Enter Radius {radius_type.split()[-1]}", min_value=1, max_value = 100, value=10)
     return address, radius_type, radius
 
 @st.experimental_fragment
